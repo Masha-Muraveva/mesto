283187de62cbbdcd431b3176a5ca3a7d15761f83
Masha Muraveva <Maria.Muraveva.S@yandex.ru>
2022-11-10 00:10:11 +0300

    Fix: внести правки от ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,15 @@
 const editButtonElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
 const popupClosedElement = document.querySelector('.popup__closed-button');
-const popupContentElement = popupElement.querySelector('.popup__content');
-const nameInput = popupContentElement.querySelector('.popup__user-name');
-const descriptionInput = popupContentElement.querySelector('.popup__user-description');
-const submitButton = popupContentElement.querySelector('.popup__save-button');
+const popupFormElement = popupElement.querySelector('.popup__form');
+const nameInput = popupFormElement.querySelector('.popup__form-data_name');
+const descriptionInput = popupFormElement.querySelector('.popup__form-data_description');
 const userName = document.querySelector('.profile__user-name');
 const userDescription = document.querySelector('.profile__user-description');
 
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  userName.textContent = nameInput.value;
-  userDescription.textContent = descriptionInput.value;
-  popupElement.classList.remove('popup_opened');
-}
-
 function openClick () {
+  nameInput.value = userName.textContent; 
+  descriptionInput.value = userDescription.textContent;
   popupElement.classList.add('popup_opened');
 }
 
@@ -23,6 +17,13 @@ function closeClick () {
   popupElement.classList.remove('popup_opened');
 }
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  userDescription.textContent = descriptionInput.value;
+  closeClick();
+}
+
 editButtonElement.addEventListener('click', openClick);
 popupClosedElement.addEventListener('click', closeClick);
-popupContentElement.addEventListener('submit', formSubmitHandler);
+popupFormElement.addEventListener('submit', formSubmitHandler);
